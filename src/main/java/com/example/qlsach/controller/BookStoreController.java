a1a876470310632dad00dc6149cbd73ed2ad5b9c
phammanh <manhpd133@gmail.com>
2022-09-26 12:00:28 +0700

    Refactor code

--- src/main/java/com/example/qlsach/controller/BookStoreController.java
+++ src/main/java/com/example/qlsach/controller/BookStoreController.java
@@ -1,16 +1,13 @@
 package com.example.qlsach.controller;
 
+import com.example.qlsach.Service.BookStoreServices;
 import com.example.qlsach.model.BookStore;
-import com.example.qlsach.reponsitory.BookStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -18,78 +15,35 @@ import java.util.Optional;
 public class BookStoreController {
 
     @Autowired
-    private BookStoreRepository bookStoreRepository;
+    private BookStoreServices bookStoreServices;
 
-    @GetMapping () // Tất cả đều có chung path là bookstore tại sao không gắn bookstore vào RequestMapping? Sửa bookstore thành book_store
+    @GetMapping ()
     public ResponseEntity<List<BookStore>> getAllBookStores(@RequestParam (required = false) String nameBookStore) {
-        try {
-            List<BookStore> bookStores = new ArrayList<>();
-            if (nameBookStore == null) {
-                bookStoreRepository.findAll().forEach(bookStores::add);
-            }// Vấn đề khoảng trắng và cách dòng bị tương tự như controler phía trên
-            else {
-                bookStoreRepository.findByNameBookStore(nameBookStore).forEach(bookStores::add);
-            }
-            if (bookStores.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(bookStores, HttpStatus.OK);
-        }catch (Exception ex) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return bookStoreServices.getAllBookStore(nameBookStore);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BookStore> getBookStoreByID(@PathVariable("id") long id) {
-        Optional <BookStore> bookStoreData = bookStoreRepository.findById(id);
-        if (bookStoreData.isPresent()) {
-            return new ResponseEntity<>(bookStoreData.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return bookStoreServices.getBookStoreByID(id);
     }
 
     @PostMapping()
     public ResponseEntity<BookStore> createBookStore(@RequestBody BookStore bookStore) {
-        try {
-            BookStore bookStoreAdd = bookStoreRepository.save(new BookStore(bookStore.getNameBookStore(), bookStore.getAddress()));
-            return new ResponseEntity<>(bookStoreAdd, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+       return bookStoreServices.createBookStore(bookStore);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<BookStore> updateBookStore(@PathVariable("id") long id, @RequestBody BookStore bookStore) {
-        Optional<BookStore> bookStoreData = bookStoreRepository.findById(id);
-        if (bookStoreData.isPresent()) {
-            BookStore bookStoreEdit = bookStoreData.get();
-            bookStoreEdit.setNameBookStore(bookStore.getNameBookStore());
-            bookStoreEdit.setAddress(bookStore.getAddress());
-            return new ResponseEntity<>(bookStoreRepository.save(bookStoreEdit), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return bookStoreServices.updateBookStore(id, bookStore);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<BookStore> deleteBookStore (@PathVariable("id") long id) {
-        try {
-            bookStoreRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception ex) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<BookStore> deleteBookStoreId (@PathVariable("id") long id) {
+       return bookStoreServices.deleteBookStoreId(id);
     }
 
     @DeleteMapping()
     public ResponseEntity<HttpStatus> deleteAllBookStore() {
-        try {
-            bookStoreRepository.deleteAll();
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+       return bookStoreServices.deleteAllBookStore();
     }
-
 }
